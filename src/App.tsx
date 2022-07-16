0a34188a7eb077e8bf559adcef8d5b62732f0c19
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-07-16 17:37:00 -0300

    Starting project styling with CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { AddTask } from "./components/AddTask"
 import { Header } from "./components/Header"
 import { TaskList } from "./components/TaksList"
 
+import './global.css'
+
 function App() {
   return (
     <>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import todoLogo from '../assets/logo.svg';
-import './Header.css';
+import styles from './Header.module.css';
 
 export function Header(){
   return (
-    <header>
+    <header className={styles.header}>
       <img src={todoLogo} />
     </header>
   )
