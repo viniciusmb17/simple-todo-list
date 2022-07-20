4e388dd904a6418b245c73d80b3ed52a180d3e6c
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-07-19 21:13:52 -0300

    feat: styling the TaskList component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,22 +1,29 @@
+import { ClipboardText } from 'phosphor-react'
+import clipboard from '../assets/clipboard.svg'
 import styles from './TaskList.module.css'
+
 
 export function TaskList() {
   return (
     <section className={styles.section}>
       <div className={styles.info}>
         <div className={styles.created}>
-          Tarefas criadas 
+          Tarefas criadas
           <div className={styles.counter}>0</div>
         </div>
         <div className={styles.done}>
-          Concluídas 
+          Concluídas
           <div className={styles.counter}>0</div>
         </div>
       </div>
-      <div>
-        <span>task icon</span>
-        <p>Você ainda não tem tarefas cadastradas</p>
-        <p>Crie tarefas e organize seus itens a fazer</p>
+      <div className={styles.content}>
+        <span>
+          <img src={clipboard} alt="clipboard image" width={56} height={56} />
+        </span>
+        <div className={styles.text}>
+          <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
       </div>
     </section>
   )
